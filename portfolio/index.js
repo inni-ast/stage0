abd096770ac66b666a0a34ed97c27c8e429ac9c7
inni-ast <inni_@tut.by>
2022-01-26 11:30:57 +0300

    init: start portfolio-part3

--- portfolio/index.js
+++ portfolio/index.js
@@ -6,17 +6,42 @@ const hamburgerMenu = document.querySelector('.header__nav');
 console.log(hamburger);
 console.log(hamburgerMenu);
 
+// открыть меню на разрешении 768px
+
 function toggleMenu() {
 	hamburger.classList.toggle('open');
 	hamburgerMenu.classList.toggle('openMenu');
 }
 hamburger.addEventListener('click', toggleMenu);
 
-
+//закрытие меню после выбора section
 function closeMenu() {
 	hamburger.classList.remove('open');
 	hamburgerMenu.classList.remove('openMenu');
 }
 const navLinks = document.querySelectorAll('.list__item');
 navLinks.forEach((el) => el.addEventListener('click', closeMenu));
+
+//смена изображений в портфолио
+const portfolioBtnWinter = document.querySelector('.winter');
+const portfolioBtnSpring = document.querySelector('.spring');
+const portfolioBtnSummer = document.querySelector('.summer');
+const portfolioBtnAutumn = document.querySelector('.autumn');
+let portfolioImages = document.querySelectorAll('.portfolio__image');
+
+portfolioBtnSummer.addEventListener('click', () => {
+	portfolioImages.forEach((img, index) => img.src = `assets/img/summer/${index + 1}.jpg`);
+});
+
+portfolioBtnWinter.addEventListener('click', () => {
+	portfolioImages.forEach((img, index) => img.src = `assets/img/winter/${index + 1}.jpg`);
+});
+
+portfolioBtnSpring.addEventListener('click', () => {
+	portfolioImages.forEach((img, index) => img.src = `assets/img/spring/${index + 1}.jpg`);
+});
+portfolioBtnAutumn.addEventListener('click', () => {
+	portfolioImages.forEach((img, index) => img.src = `assets/img/autumn/${index + 1}.jpg`);
+});
+
 
